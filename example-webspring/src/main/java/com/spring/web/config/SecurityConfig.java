eb866bee55475931586350d4f51d7d49857bb25f
Senthil Balan <senthil.balan@Aspiresys.com>
2018-04-19 18:27:19 +0530

    InMemory authentication added

--- example-webspring/src/main/java/com/spring/web/config/SecurityConfig.java
+++ example-webspring/src/main/java/com/spring/web/config/SecurityConfig.java
@@ -1,11 +1,16 @@
 package com.spring.web.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -15,18 +20,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().
-		antMatchers("/app/admin/**").access("hasRole('ROLE_ADMIN')").
-		antMatchers("/app/user/**").access("hasRole('ROLE_USER')").
+		antMatchers("/app/**").hasRole("admin").
+		/*.authenticated().*/
 		and().
-		formLogin();
+		formLogin().defaultSuccessUrl("/app/bookstore/category");
 	}
+	
+	
+	
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		
+		  auth.inMemoryAuthentication().withUser("admin").password("admin");
+		
+	}
+	
 	
 	@Configuration
 	protected static class AuthenticationConfiguration extends GlobalAuthenticationConfigurerAdapter {
 		@Override
 		public void init(AuthenticationManagerBuilder auth) throws Exception {
 			
+			UserDetails  user = User.builder().username("admin").password(passwordEncoder().encode("admin")).roles("admin").build();
+			
+			auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).withUser(user);
 
+		}
+		
+		@Bean
+		public PasswordEncoder passwordEncoder() {
+		    return new BCryptPasswordEncoder();
 		}
 	} 
 	
